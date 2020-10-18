f1617904864926ffa839cd4c44b6079bd117c3dd
lakshay-droid <57001958+lakshay-droid@users.noreply.github.com>
2020-10-18 10:20:46 +0530

    Added ISBN detector

---
+++ isbn.java
@@ -0,0 +1,34 @@
+
+
+import java.util.Scanner;
+
+public class isbn {
+
+    public static void main(String[] args) {
+        System.out.println("enter the 10 digit ISBN");
+        long store[];
+        long sum=0;
+        store = new long[10];
+        Scanner scan = new Scanner(System.in);
+        String num= scan.next();
+        long num1 = Long.parseLong(num);
+        if(num.length()==10){
+            for (int i=9;i>=0;i--){
+                store[i]=(num1%10);
+                num1=(num1/10);
+                sum=sum+(store[i]*(i+1));
+            }
+            System.out.println(sum);
+           if ((sum%11)==0) {
+               System.out.println("Legal ISBN");
+           }
+            else{
+                System.out.println("Illegal ISBN");
+            }
+
+        }
+        else{
+            System.out.println("Illegal ISBN");
+        }
+    }
+}
